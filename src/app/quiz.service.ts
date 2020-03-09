bf3e7db3275a3461dc06aa8e77c50fbc7a953d1c
fred.macharia <fred.macharia@turnkeyafrica.com>
2020-03-09 09:17:48 +0300

    Added update and delete methods

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -9,14 +9,14 @@ import { environment } from 'src/environments/environment';
 })
 export class QuizService {
   api = `${environment.url}`;
+  token = window.sessionStorage.getItem('token');
 
   constructor(
     private http: HttpClient
   ) { }
 
   save(quiz: Quiz[]): Observable<any> {
-    const token = window.sessionStorage.getItem('token');
-    return this.http.post(`${this.api}/questions?token=${token}`, quiz);
+    return this.http.post(`${this.api}/questions?token=${this.token}`, quiz);
   }
 
   respond(responses: Quiz[]): Observable<any> {
@@ -29,5 +29,13 @@ export class QuizService {
 
   login(data: { username: string, password: string }): Observable<any> {
     return this.http.post(`${this.api}/login`, data);
+  }
+
+  update(id: number, question: string): Observable<any> {
+    return this.http.put(`${this.api}/questions?token=${this.token}`, { id, question });
+  }
+
+  delete(id: number): Observable<any> {
+    return this.http.delete(`${this.api}/questions?token=${this.token}&id=${id}`);
   }
 }
